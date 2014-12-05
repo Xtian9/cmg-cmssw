80935ae311ad76e3bcb309412f3235cd5ff285c8
aelwood <adam.elwood09@imperial.ac.uk>
2014-12-05 14:52:07 +0000

    Started trying to get it to work at imperial

--- CMGTools/TTHAnalysis/python/config/config_cfi.py
+++ CMGTools/TTHAnalysis/python/config/config_cfi.py
@@ -8,15 +8,15 @@ import CMGTools.RootTools.fwlite.Config as cfg
 alphaTPSet = cfg.CFG(
 puRegime = 'PU40bx50',
 #puRegime = 'PU20bx25',
-cutFlow = 'MultiJetEnriched',
-#cutFlow = 'Signal',
+#cutFlow = 'MultiJetEnriched',
+cutFlow = 'Signal',
 #cutFlow = 'SingleMu',
 #cutFlow = 'DoubleMu',
 #cutFlow = 'SinglePhoton',
 #cutFlow = 'SingleEle',
 #cutFlow = 'DoubleEle',
 #cutFlow = 'Test',
-test =  0,
+test =  1,
 limitFiles = False,
 )
 
